713ef47563e24ad5715134dadbcf597d46c2de2f
kartafil <allan.abasov@gmail.com>
2024-07-31 14:00:07 +0300

    ref: clsx added, markup minor changes

--- src/pages/favorites-page/favorites-page.tsx
+++ src/pages/favorites-page/favorites-page.tsx
@@ -6,7 +6,7 @@ function FavoritesPage(): JSX.Element {
           <div className="header__wrapper">
             <div className="header__left">
               <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"></img>
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
               </a>
             </div>
             <nav className="header__nav">
@@ -50,7 +50,7 @@ function FavoritesPage(): JSX.Element {
                     </div>
                     <div className="favorites__image-wrapper place-card__image-wrapper">
                       <a href="#">
-                        <img className="place-card__image" src="img/apartment-small-03.jpg" width="150" height="110" alt="Place image"></img>
+                        <img className="place-card__image" src="img/apartment-small-03.jpg" width="150" height="110" alt="Place image"/>
                       </a>
                     </div>
                     <div className="favorites__card-info place-card__info">
@@ -82,7 +82,7 @@ function FavoritesPage(): JSX.Element {
                   <article className="favorites__card place-card">
                     <div className="favorites__image-wrapper place-card__image-wrapper">
                       <a href="#">
-                        <img className="place-card__image" src="img/room-small.jpg" width="150" height="110" alt="Place image"></img>
+                        <img className="place-card__image" src="img/room-small.jpg" width="150" height="110" alt="Place image"/>
                       </a>
                     </div>
                     <div className="favorites__card-info place-card__info">
@@ -125,7 +125,7 @@ function FavoritesPage(): JSX.Element {
                   <article className="favorites__card place-card">
                     <div className="favorites__image-wrapper place-card__image-wrapper">
                       <a href="#">
-                        <img className="place-card__image" src="img/apartment-small-04.jpg" width="150" height="110" alt="Place image"></img>
+                        <img className="place-card__image" src="img/apartment-small-04.jpg" width="150" height="110" alt="Place image"/>
                       </a>
                     </div>
                     <div className="favorites__card-info place-card__info">
@@ -161,7 +161,7 @@ function FavoritesPage(): JSX.Element {
       </main>
       <footer className="footer container">
         <a className="footer__logo-link" href="main.html">
-          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"></img>
+          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
         </a>
       </footer>
     </div>
