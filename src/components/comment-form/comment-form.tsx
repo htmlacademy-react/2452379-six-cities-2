402a128ca4e4b795c3e465d6310ed4eb41ba3f60
kartafil <allan.abasov@gmail.com>
2024-09-09 19:44:18 +0300

    fix: CommentForm

--- src/components/comment-form/comment-form.tsx
+++ src/components/comment-form/comment-form.tsx
@@ -54,7 +54,7 @@ export default function CommentForm() {
           })()
         }
       </div>
-      <textarea onChange={handleInputChange}className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" minLength={MIN_COMMENT_LENGTH}></textarea>
+      <textarea onChange={handleInputChange}className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" value={formData.review}></textarea>
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
