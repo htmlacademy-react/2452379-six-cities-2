6cd68c734074e9fe0bbdbf8d86e82e685bda3591
kartafil <allan.abasov@gmail.com>
2024-09-25 14:14:14 +0300

    WIP: user auth

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,10 @@ import { Provider } from 'react-redux';
 import App from './components/app/app';
 import store from './store';
 import { getOffersThunk } from './store/extra/offers-actions';
+import { tryAuth } from './store/extra/user-actions';
 
 store.dispatch(getOffersThunk());
+store.dispatch(tryAuth());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
